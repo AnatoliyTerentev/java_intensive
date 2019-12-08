8a91303c6b83494a9b9aab854e6c5c41bca8b5a5
Anatoliy_Terentev <ant_x@list.ru>
2019-12-08 22:23:07 +0400

    commit FactorialCount

---
+++ src/FactorialCount.java
@@ -0,0 +1,10 @@
+public class FactorialCount{
+
+    public static void main(String[] args) {
+        int natNumber = 1;
+           for (int i = 1; i <= 6; i++) {
+         natNumber = natNumber * i;
+        }
+        System.out.println(natNumber);
+    }
+}
